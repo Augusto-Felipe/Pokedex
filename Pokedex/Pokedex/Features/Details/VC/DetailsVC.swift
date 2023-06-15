3f587254c654dd08b99749763730e1f98fb2c736
Felipe Augusto <augusto_felipe@icloud.com>
2023-06-15 15:27:25 -0300

    enum pokemon color background type

--- Pokedex/Pokedex/Features/Details/VC/DetailsVC.swift
+++ Pokedex/Pokedex/Features/Details/VC/DetailsVC.swift
@@ -7,6 +7,20 @@
 
 import UIKit
 
+enum PokemonType {
+    case poison
+    case fire
+    case flying
+    case water
+    case ground
+    case grass
+    case electric
+    case fairy
+    case normal
+    case ice
+    case psychic
+}
+
 class DetailsVC: UIViewController {
     
     var screen: DetailsScreen?
@@ -21,10 +35,52 @@ class DetailsVC: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .brown
         setupPokemonDetails()
-        //        self.configBackgroundColorPokemonType()
     }
     
     public func setupPokemonDetails() {
+        let typeMappings: [String: PokemonType] = [
+            "poison": .poison,
+            "fire": .fire,
+            "flying": .flying,
+            "water": .water,
+            "ground": .ground,
+            "grass": .grass,
+            "electric": .electric,
+            "fairy": .fairy,
+            "normal": .normal,
+            "ice": .ice,
+            "psychic": .psychic
+        ]
+        
+        if let pokemonType = typeMappings[pokemon?.type ?? ""] {
+            switch pokemonType {
+            case .poison:
+                view.backgroundColor = .poisonTypeColor
+            case .fire:
+                view.backgroundColor = .fireTypeColor
+            case .flying:
+                view.backgroundColor = .flyingTypeColor
+            case .water:
+                view.backgroundColor = .waterTypeColor
+            case .ground:
+                view.backgroundColor = .groundTypeColor
+            case .grass:
+                view.backgroundColor = .grassTypeColor
+            case .electric:
+                view.backgroundColor = .electricTypeColor
+            case .fairy:
+                view.backgroundColor = .fairyTypeColor
+            case .normal:
+                view.backgroundColor = .normalTypeColor
+            case .ice:
+                view.backgroundColor = .iceTypeColor
+            case .psychic:
+                view.backgroundColor = .psychicTypeColor
+            }
+        } else {
+            print("Tipo de Pokémon inválido")
+        }
+        
         if let urlPokemon: URL = URL(string: pokemon?.imageURL ?? "") {
             self.screen?.pokemonImageView.af.setImage(withURL: urlPokemon, placeholderImage: UIImage(named: "threeButtons"))
         }
