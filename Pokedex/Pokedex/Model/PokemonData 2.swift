19cb42e175184ce5075896c3bc681490227237b7
Felipe Augusto <augusto_felipe@icloud.com>
2023-09-16 17:57:11 -0300

    [UPDATE] merge into develop

---
+++ Pokedex/Pokedex/Model/PokemonData 2.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Felipe Augusto Correia on 02/04/23.
+//
+
+import Foundation
+
+// MARK: - PokemonData
+struct PokemonData: Codable {
+    var pokemon: [Pokemon]?
+}
+
+
+
---
+++ Pokedex/Pokedex/Service/PokemonService 2.swift
@@ -0,0 +1,36 @@
+//
+//  PokemonManager.swift
+//  Pokedex
+//
+//  Created by Felipe Augusto Correia on 02/04/23.
+//
+
+import Foundation
+import Alamofire
+
+enum ErrorProfile: Swift.Error {
+    case errorRequest(AFError)
+}
+
+struct PokemonService {
+    func getPokemonDataAlamofire(completion: @escaping (PokemonData?, Error?) -> Void) {
+        let url: String = "https://run.mocky.io/v3/3d1b35c4-205d-4547-99f1-bf1d3df95bd2"
+        
+        AF.request(url, method: .get).validate().responseDecodable(of: PokemonData.self) { response in
+            
+            debugPrint(response)
+            
+            switch response.result {
+            case .success(let sucess):
+                print("SUCESS -> \(#function)")
+                completion(sucess, nil)
+            case .failure(let error):
+                print("ERROR -> \(#function)")
+                completion(nil, ErrorProfile.errorRequest(error))
+            }
+        }
+    }
+}
+
+
+
